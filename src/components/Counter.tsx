224092b6ba510d4f16207d51a3a5663e22e6b6e4
Abhirup <abhidatta146@gmail.com>
2023-04-22 23:32:45 +0530

    derived signal

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,15 +2,19 @@ import {createSignal, createEffect} from 'solid-js';
 
 const Counter = ()=>{
     const [count, setCount] = createSignal(0);
+    const double = ()=> count()*2;
 
     createEffect(()=>{
         console.log("Hello", count());
     })
+
+    console.log("component");
     return (
         <div>
             <button onClick={()=> setCount(count()+1)}>Increment</button>
             <button onClick={()=> setCount(count()-1)}>Decrement</button>
             <p>Count {count()}</p>
+            <p>Double {double()}</p>
         </div>
     )
 
